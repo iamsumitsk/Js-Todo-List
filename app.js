f6cb5a28a2b0dd7113ea760b39d7b399fc213e33
iamsumitsk <sk.sumit0852@gmail.com>
2019-10-23 20:45:28 +0530

    functionality added

---
+++ app.js
@@ -0,0 +1,55 @@
+const addForm = document.querySelector('.add');
+const search = document.querySelector('.serach input');
+const list = document.querySelector('.todos');
+
+const generatetemplete = todo => {
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+    list.innerHTML += html;
+};
+
+const filterTodos = term => {
+
+    //add filtered class
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLocaleLowerCase().includes(term))
+        .forEach(todo => todo.classList.add('filtered'));
+
+        //remove filter class
+        Array.from(list.children)
+        .filter(todo => todo.textContent.toLocaleLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+};
+
+//add todos event
+addForm.addEventListener('submit', e => {
+    
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if(todo.length) {
+        generatetemplete();
+        addForm.reset();
+    }
+});
+
+//delete todos event
+list.addEventListener('click', e => {
+
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+//filter todos event
+search.addEventListener('keyup', () => {
+
+    const term = search.value.trim().toLocaleLowerCase();
+    filterTodos(term);
+});
+    
+    
